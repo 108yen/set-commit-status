df9508e76ae207d36206d55f84b43ddbfa790f9d
108yen <kazuking.1911@gmail.com>
2025-04-26 15:50:12 +0900

    test: setup coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     clearMocks: true,
+    coverage: {
+      include: ["src/**"],
+      provider: "v8",
+    },
     include: ["./test/**"],
     mockReset: true,
     restoreMocks: true,
